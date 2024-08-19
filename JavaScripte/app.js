9e4caae4d1fc3ebd898cb9e63a5f7c423e9a7f73
inmaj hossain <inmaj670@gmail.com>
2024-08-19 13:12:52 +0600

    Practice

---
+++ JavaScripte/app.js
@@ -0,0 +1,126 @@
+// // variable
+// 1. variable
+// 2. let
+// 3. const
+
+// Using var
+
+// var name, job, age, title;
+
+// job = "Marketing;
+// var job = "marketing"
+// console.log(job);
+
+// Using let
+
+// person = "nadim";
+// let age, job, title;
+
+// job = "programmer"
+// job = "Backend" 
+// // backend working because of last assing value is show always 
+// title = "Development"
+
+// let person = "Zohan"
+
+// console.log(person, job, title)
+
+// For Var Scope
+
+// var carModel = 2024
+// {
+//     var carModel = 2030
+//     console.log(carModel)
+// }
+// console.log(carModel)
+
+// For let Scope
+
+// let carModel = 2040
+// {
+//     let carModel = 2050
+//     console.log(carModel)
+// }
+// console.log(carModel)
+
+// Dynamic Value
+
+let countryName = "Bangladesh"
+let city = "Dhaka"
+let sentance =  `${city} is capital of ${countryName}`
+console.log(sentance)
+
+// Bollean
+let heMarried = true;
+// const heMarried = false;
+console.log(typeof heMarried);
+
+// Undefider
+
+let car
+console.log(car)
+
+// null
+let sanan = null
+console.log(typeof sanan)
+
+// NaN Not a Number
+let counter = 35
+console.log(counter * "Hi")
+
+
+
+
+// Functionality
+
+
+console.log(40**3)
+// Example : 40 * 40 * 40
+
+// Modulas
+
+console.log(10 % 3)
+// Example : Vagses Bosbe
+
+// Increment & Decrement ++ hole 1 add hobe -- hole 1 Minus hobe
+
+let salary = 500;
+salary++;
+salary++;
+salary++;
+salary++;
+salary++;
+salary--;
+salary--;
+console.log(salary)
+
+// += function
+// number er value ke + sign hole eksathe 2 bar jog kora or * sign hole gun kora
+
+let number = 50
+number += number
+console.log(number)
+
+
+// == sign is true/false checker
+
+let zohan = "married"
+console.log(zohan == "unmarried")
+console.log(zohan != "unmarried")
+// not equal sign is !=
+console.log(zohan == "married")
+
+
+// === sign is strictly true/false checker
+
+let mark = "20"
+console.log(mark === 20)
+
+/* < and > */
+
+console.log(20 < 10)
+console.log(20 > 10)
+console.log(20 <= 20)
+
+
+
